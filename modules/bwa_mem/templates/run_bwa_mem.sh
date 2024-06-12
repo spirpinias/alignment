c72f5e8690a91cf8532d2ff4316c15cbaaddb547
Stephen Pirpinias <stpirpinias@gmail.com>
2024-06-12 14:25:59 -0400

    Update run_bwa_mem.sh

--- modules/bwa_mem/templates/run_bwa_mem.sh
+++ modules/bwa_mem/templates/run_bwa_mem.sh
@@ -1,12 +1,6 @@
 #!/usr/bin/env bash
 
-reads=$(find -L ./ -name "*_R1.fastq.gz" | head -1)
-index_prefix=$(find -L ./ -name "*.amb")
-
-prefix=$(basename -s _R1.fastq.gz $reads)
-index=$(basename -s .amb $index_prefix)
-
 bwa mem \
 -t !{task.cpus} \
-$index \
-!{reads} | samtools view -b - > $prefix.bam
+!{params.prefix_genome} \
+!{reads} | samtools view -b - > !{id}.bam
